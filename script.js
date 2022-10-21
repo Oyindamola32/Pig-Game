6222e158f9ac5e140ab75e7b9621e951bbc23f69
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-21 21:55:29 +0100

    modify script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
+
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. Generating a random dice roll
@@ -31,8 +33,11 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     current0El.textContent = currentScore; //CHANGE LATER
   } else {
     //switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
